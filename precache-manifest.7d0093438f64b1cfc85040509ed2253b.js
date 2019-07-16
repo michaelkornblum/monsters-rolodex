a81398592a24de034cf49fe80321d103cb98611f
Michael Kornblum <mkornblum71@gmail.com>
2019-07-16 12:20:31 -0500

    Updates

---
+++ precache-manifest.7d0093438f64b1cfc85040509ed2253b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5639571415f6b362c6be980dfef31fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0e3f30bb35f934e3d121",
+    "url": "/monsters-rolodex/static/css/main.067d644d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0e3f30bb35f934e3d121",
+    "url": "/monsters-rolodex/static/js/main.308e231d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
